fd2dcce417437c22a653615d0e9857f7ee9b04ed
Umalat <dukuev037@mail.ru>
2023-10-13 18:12:41 +0300

    added package uniq

--- uniq.go
+++ uniq.go
@@ -5,139 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
+	"uniq"
 )
 
-type Options struct {
-	c bool
-	d bool
-	u bool
-	f int
-	s int
-	i bool
-}
+func CheckInput(opts uniq.Options) {
 
-func isEqual(str1 string, str2 string, opts Options) bool {
-	if opts.i == true {
-		return strings.EqualFold(str1, str2)
-	} else {
-		return str1 == str2
-	}
-}
-
-func CollapseLines(scanner *bufio.Scanner, writer *bufio.Writer, opts Options) {
-	lines := make([]string, 0)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if opts.i == true {
-		for i := 0; i < len(lines); i++ {
-			lines[i] = strings.ToLower(lines[i])
-		}
-	}
-	for i := 0; i < len(lines); i++ {
-		if opts.f != 0 {
-			words := strings.Fields(lines[i])
-			if len(words) > opts.f {
-				result := strings.Join(words[opts.f:], " ")
-				lines[i] = result
-			} else {
-				fmt.Println("Недостаточно слов в строке для вывода")
-				return
-			}
-		}
-	}
-
-	for i := 0; i < len(lines); i++ {
-		if opts.s != 0 {
-			str := lines[i]
-			if len(str) > opts.s {
-				result := str[opts.s:]
-				lines[i] = result
-			} else {
-				fmt.Println("Недостаточно символов в строке для вывода")
-				return
-			}
-
-		}
-	}
-	cnt := 1
-	prevLine := lines[0]
-	for i := 1; i < len(lines); i++ {
-		line := lines[i]
-		if isEqual(line, prevLine, opts) == true {
-			cnt++
-		} else {
-			if opts.c == true {
-				writer.WriteString(strconv.Itoa(cnt) + " " + prevLine + "\n")
-			} else {
-				if opts.d == true {
-					if cnt > 1 {
-						writer.WriteString(prevLine + "\n")
-					}
-				} else {
-					if opts.u == true {
-						if cnt == 1 {
-							writer.WriteString(prevLine + "\n")
-						}
-					} else {
-						writer.WriteString(prevLine + "\n")
-					}
-				}
-
-			}
-			cnt = 1
-		}
-		prevLine = line
-	}
-	if opts.c == true {
-		writer.WriteString(strconv.Itoa(cnt) + " " + prevLine + "\n")
-	} else {
-		if opts.d == true {
-			if cnt > 1 {
-				writer.WriteString(prevLine + "\n")
-			}
-		} else {
-			if opts.u == true {
-				if cnt == 1 {
-					writer.WriteString(prevLine + "\n")
-				}
-			} else {
-				writer.WriteString(prevLine + "\n")
-			}
-		}
-	}
-	writer.Flush()
-}
-
-func ParseFlags(opts Options) Options {
-	flag.BoolVar(&opts.c, "c", false, "add number of lines")
-	flag.BoolVar(&opts.d, "d", false, "stdout repeating lines")
-	flag.BoolVar(&opts.u, "u", false, "stdout uniq lines")
-	flag.IntVar(&opts.f, "f", 0, "don't consider first num fields")
-	flag.IntVar(&opts.s, "s", 0, "don't consider first num symbols")
-	flag.BoolVar(&opts.i, "i", false, "")
-	flag.Parse()
-	return opts
-}
-
-func CheckInput(opts Options) {
-	cnt := 0
-	if opts.c == true {
-		cnt++
-	}
-	if opts.d == true {
-		cnt++
-	}
-	if opts.u == true {
-		cnt++
-	}
-	if cnt > 1 {
-		fmt.Println("You can use only one of c/d/u arguments")
-		return
-	}
 	if len(flag.Args()) > 2 {
 		fmt.Println("Maximum number of arguments = 2! ")
 		return
@@ -163,13 +35,12 @@ func CheckInput(opts Options) {
 	}
 	scanner := bufio.NewScanner(input)
 	writer := bufio.NewWriter(output)
-	CollapseLines(scanner, writer, opts)
-
+	uniq.CollapseLines(scanner, writer, opts)
 }
 
 func main() {
-	var opts Options
-	opts = ParseFlags(opts)
+	var opts uniq.Options
+	opts = uniq.ParseFlags(opts)
 	CheckInput(opts)
 	return
 }
